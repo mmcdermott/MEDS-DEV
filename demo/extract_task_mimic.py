8732fa09584942f7c369c795fee657eccdb79102
kamilest <stankeviciute.kamile@gmail.com>
2024-12-16 12:51:28 -0800

    Extract task for MIMIC-IV demo

--- demo/extract_task_mimic.py
+++ demo/extract_task_mimic.py
@@ -1,6 +1,6 @@
 #%%
 # MEDS_DEV_DEMO_CONDA_ENV = "meds-dev-demo"
-MEDS_DEV_DEMO_CONDA_ENV = "meds-dev"
+# MEDS_DEV_DEMO_CONDA_ENV = "meds-dev"
 
 #%%
 # !conda init bash
@@ -11,19 +11,11 @@ MEDS_DEV_DEMO_CONDA_ENV = "meds-dev"
 # Install MEDS-DEV and dependencies
 
 #%%
-!pwd
-#%%
 !git clone https://github.com/mmcdermott/MEDS-DEV.git
 !cd ./MEDS-DEV && pip install -e .
 
 #%% [markdown]
 # TODO Install any model-related dependencies.
-
-#%% [markdown]
-# TODO Install MEDS Evaluation
-#%%
-# !git clone https://github.com/kamilest/meds-evaluation.git
-# !cd ./meds-evaluation && pip install -e .
 
 # %%
 MEDS_DEV_PROJECT_ROOT = "."
@@ -37,20 +29,3 @@ TASK_NAME = "mortality/in_icu/first_24h"
 
 # %%
 !./MEDS-DEV/src/MEDS_DEV/helpers/extract_task.sh {MEDS_DEV_PROJECT_ROOT} {DATASET_NAME} {TASK_NAME}
-
-# %% [markdown]
-# ## Training MEDS-TAB
-
-# %%
-!pip uninstall es-aces -y # TODO ???
-!pip install meds-tab
-
-# %%
-MIMICIV_MEDS_DIR = "./content/meds/"
-OUTPUT_TABULARIZATION_DIR="./content/tabularized/"
-TASK_DIR="./content/tasks/"
-TASK_NAME="in_hospital_3d_los_after_48h"
-OUTPUT_MODEL_DIR="./content/output/meds_tab/"
-
-# %%
-!meds-tab-describe input_dir={MIMICIV_MEDS_DIR}/data output_dir={OUTPUT_TABULARIZATION_DIR}
