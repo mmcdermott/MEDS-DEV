ec4c68b411441796fb9ea0a45a8ef7ad6d5ae190
kamilest <stankeviciute.kamile@gmail.com>
2024-12-16 13:18:53 -0800

    Extract task from within MIMIC-IV demo meds directory

--- demo/extract_task_mimic_demo.py
+++ demo/extract_task_mimic_demo.py
@@ -1,12 +1,3 @@
-#%%
-# MEDS_DEV_DEMO_CONDA_ENV = "meds-dev-demo"
-# MEDS_DEV_DEMO_CONDA_ENV = "meds-dev"
-
-#%%
-# !conda init bash
-# !conda create -n {MEDS_DEV_DEMO_CONDA_ENV} python=3.12
-# !conda activate {MEDS_DEV_DEMO_CONDA_ENV}
-
 #%% [markdown]
 # Install MEDS-DEV and dependencies
 
@@ -18,7 +9,11 @@
 # TODO Install any model-related dependencies.
 
 # %%
-MEDS_DEV_PROJECT_ROOT = "."
+import os
+ROOT_DIR=f"{os.getcwd()}/work_dir/mimiciv_demo"
+TUTORIAL_DIR = f"{ROOT_DIR}/MIMIC-IV_Example"
+
+MIMICIV_DEMO_MEDS_DATA_DIR = f"{ROOT_DIR}/meds"
 DATASET_NAME = "MIMIC-IV"
 TASK_NAME = "mortality/in_icu/first_24h"
 
@@ -28,4 +23,5 @@ TASK_NAME = "mortality/in_icu/first_24h"
 # processing the data to create the label files.
 
 # %%
-!./MEDS-DEV/src/MEDS_DEV/helpers/extract_task.sh {MEDS_DEV_PROJECT_ROOT} {DATASET_NAME} {TASK_NAME}
+!./MEDS-DEV/src/MEDS_DEV/helpers/extract_task.sh {MIMICIV_DEMO_MEDS_DATA_DIR} {DATASET_NAME} {TASK_NAME}
+# %%
