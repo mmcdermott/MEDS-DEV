8e2006a1981bb1fade9d73a3b7c3d53ccac7f922
Matthew McDermott <mattmcdermott8@gmail.com>
2025-01-21 15:01:19 -0500

    Got the test working up through the running of the command -- not the command itself.

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -6,4 +6,4 @@ from tests.utils import run_command
 
 @pytest.mark.parametrize("dataset", DATASETS)
 def test_dataset(dataset: str):
-    run_command(f"meds-dev-dataset dataset={dataset}")
+    run_command("meds-dev-dataset", test_name=f"Build {dataset}", hydra_kwargs={"dataset": dataset})
