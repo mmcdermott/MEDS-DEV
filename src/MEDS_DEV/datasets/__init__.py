317627800cdc8adc234b563a68b1b2807a96845d
Matthew McDermott <mattmcdermott8@gmail.com>
2025-01-21 15:34:44 -0500

    Got more functionality working; failure now due to lack of installation.

--- src/MEDS_DEV/datasets/__init__.py
+++ src/MEDS_DEV/datasets/__init__.py
@@ -1,11 +1,20 @@
 from importlib.resources import files
 
+from omegaconf import OmegaConf
+
 dataset_files = files("MEDS_DEV.datasets")
 CFG_YAML = files("MEDS_DEV.configs") / "_build_dataset.yaml"
 
-DATASETS = []
+DATASETS = {}
 for path in dataset_files.iterdir():
-    if path.is_dir() and (path / "commands.yaml").exists():
-        DATASETS.append(path.name)
+    if not (path.is_dir() and (path / "commands.yaml").exists()):
+        continue
+
+    commands = OmegaConf.load(path / "commands.yaml")
+    requirements_path = path / "requirements.txt"
+    DATASETS[path.name] = {
+        "commands": commands,
+        "requirements": requirements_path if requirements_path.exists() else None,
+    }
 
 __all__ = ["DATASETS", "CFG_YAML"]
--- src/MEDS_DEV/datasets/__main__.py
+++ src/MEDS_DEV/datasets/__main__.py
@@ -1,7 +1,23 @@
+import contextlib
+import subprocess
+import tempfile
+from pathlib import Path
+
 import hydra
 from omegaconf import DictConfig
 
 from . import CFG_YAML, DATASETS
+
+
+@contextlib.contextmanager
+def build_environment(cfg: DictConfig) -> Path:
+    if cfg.temp_dir is None:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            yield Path(temp_dir)
+    else:
+        temp_dir = Path(cfg.temp_dir)
+        temp_dir.mkdir(exist_ok=True, parents=True)
+        yield temp_dir
 
 
 @hydra.main(version_base=None, config_path=str(CFG_YAML.parent), config_name=CFG_YAML.stem)
@@ -9,4 +25,17 @@ def main(cfg: DictConfig):
     if cfg.dataset not in DATASETS:
         raise ValueError(f"Dataset {cfg.dataset} not currently configured!")
 
-    raise NotImplementedError("This is a placeholder function. Please implement the functionality.")
+    commands = DATASETS[cfg.dataset]["commands"]
+
+    build_cmd = commands["build_demo"] if cfg.demo else commands["build_full"]
+
+    with build_environment(cfg) as build_temp_dir:
+        build_cmd = build_cmd.format(output_dir=cfg.output_dir, temp_dir=str(build_temp_dir.resolve()))
+        build_command_out = subprocess.run(build_cmd, shell=True, cwd=build_temp_dir, capture_output=True)
+
+        command_errored = build_command_out.returncode != 0
+        if command_errored:
+            raise RuntimeError(
+                f"Build {cfg.dataset} command {build_cmd} failed with exit code "
+                f"{build_command_out.returncode}:\n{build_command_out.stderr.decode()}"
+            )
